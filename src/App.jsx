593bbd91d4dcf5e90ca3ca234b8607a07865e932
Stina Steves <stinasteves@Mac.fritz.box>
2026-05-14 13:52:43 +0200

    Add dark gray logo variant

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from 'react'
 import HeroVideo from '../public/video/230719_Bottrop-KUB.mp4'
 import Logo from '../public/Logo_grau.jpeg'
 import Logo2 from '../public/Logo_blau.jpeg'
+import Logo3 from '../public/Logo_grau_schwarz.jpeg'
 import Projects from './components/Projects'
 import './App.css'
 
@@ -51,6 +52,7 @@ function App() {
       <div className='logoWrapper' onMouseEnter={showMenu} onMouseLeave={hideMenuDelayed}>
         <img src={Logo} alt="Logo" className={`logo ${menuVisible ? 'menu-open' : ''}`} />
             <img src={Logo2} alt="Logo2" className={`logo ${menuVisible ? 'menu-open' : ''}`} />
+                        <img src={Logo3} alt="Logo3" className={`logo ${menuVisible ? 'menu-open' : ''}`} />
         <nav className={`menuItems ${menuVisible ? 'visible' : ''}`}>
           <a href='#about'>Business</a>
           <a href='#projekte'>Aktuelle Projekte</a>
