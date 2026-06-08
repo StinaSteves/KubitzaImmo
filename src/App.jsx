d9c418cfd06d4626c199faf550c08dedd9f9a9e1
Stina Steves <stinasteves@Mac.fritz.box>
2026-06-08 12:58:10 +0200

    Remove temporary homepage notice

--- src/App.jsx
+++ src/App.jsx
@@ -24,26 +24,7 @@ function Home() {
   </div>
 </div>
 </div>
-<h1>Hey Papa, wenn du das hier ließt schlafe ich vermutlich noch weil ich die ganze nacht hierdran gearbeitet habe. <br />
-Bitte bleib jetzt cool, aber ich muss dir was sagen... <br />
-deine website wurde gelöscht. Unter der domain kubitza-immobilien.de ist keine website mehr verfügbar <br />
-Das heißt das deine Firma gerade nicht online präsent ist. <br />
-es ist mir gestern abend aufgefallen als ich die Seite aufrufen wollte um die Bilder und Texte zu kopieren,
-da stand dann nur noch "Diese Seite ist nicht erreichbar" da. <br />
-Vermutlich ist das am Donnerstag passiert als miacheal deinen domain umzug veranlasst hat, die gute nachricht
-ist das es wohl funktioniert hat <br />
-die schlechte nachricht ist das er kein buffering eingebaut hat und deshalb die seite jetzt offline ist <br />
-aber keine panik wir kriegen das hin! Ich habe die ganze nacht an der website hier gearbeitet und eine kopie
-aus dem netz von deiner alten seite gesucht <br />
-damit ich die bilder und texte übernehmen konnte. Das impressum, der footer und der datenschutz sind fertig,
-die navigation funktioniert <br />
-und ich habe einige referenzprojekte bereits auf die seite implimentiert.
-deine texte und bilder sind soweit eingebaut und sobald ich wach werde, rufe ich dich an und wir besprechen
-was noch gemacht werden muss damit wir die website morgen online nehmen können.
-sie wird dann noch nicht perfekt fertig sein, aber auf jeden fall funktionieren und was noch fehlt können
-wir dann nachtragen <br />
-also kein stress, wir schaffen das! alles wird gut
-(dieser text ist nicht permanent und ich lösche ihn heute mittag wieder)</h1>
+
 <Projects />
 <Reverenzen />
 <Footer />
